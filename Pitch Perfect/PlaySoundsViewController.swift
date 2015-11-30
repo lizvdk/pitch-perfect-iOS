f7cf6fbc6931fa502b8462e9e8a1e5482362b15c
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-11-30 15:03:15 -0500

    Play audio fast and slow

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -17,13 +17,27 @@ class PlaySoundsViewController: UIViewController {
         if let filePathUrl = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3") {
             audioPlayer = try!
 AVAudioPlayer(contentsOfURL: filePathUrl)
+            audioPlayer.enableRate = true
         } else {
             print("Error getting file")
         }
     }
     
     @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0.0
+        audioPlayer.rate = 0.5
         audioPlayer.play()
     }
 
+    @IBAction func playFastAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0.0
+        audioPlayer.rate = 2.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopPlayingAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
 }
